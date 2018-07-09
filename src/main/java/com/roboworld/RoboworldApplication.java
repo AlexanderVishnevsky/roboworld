a9c4bb272906c0074553755fbe8b499c8fb0f5e5
Unknown <b.boy.cherry@gmail.com>
2018-07-09 16:22:18 +0300

    basic structure of the app

---
+++ src/main/java/com/roboworld/RoboworldApplication.java
@@ -0,0 +1,12 @@
+package com.roboworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboworldApplication.class, args);
+	}
+}
